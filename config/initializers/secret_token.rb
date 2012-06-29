5a659da07dad6769cf83a3101c2e53b6c48d545d
Your Name <stephen.creedon@rapidappsgroup.com>
2012-06-29 10:17:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mousetrap::Application.config.secret_token = 'a5f1c90d39b6265b84d1234d2e9f39d4bb2d6495c6b75f97a7d29d5473ad636d187f57e859419cf867916e2471e1c7707d82cbdf10b5610396010322160881aa'
